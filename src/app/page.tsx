8e021439ec8043d18f5bb4ebbc70d05e8005823a
iam8uman <iam8uman@gmail.com>
2024-01-07 16:22:14 +0545

    header and footer completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Footer from '@/components/Footer'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
       main page
+      <Footer />
     </div>
   )
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import { Twitter } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export default function Footer() {
@@ -7,6 +8,7 @@ export default function Footer() {
     <section className="absolute bottom-0 w-full bg-blue-400 py-8 rounded-lg flex justify-between">
       <div className="container relative z-10  px-4 mx-36">
         <div className="-m-8 flex flex-wrap items-center justify-between">
+          <Link href='/history'>
           <Image
             src="/logo2x.png"
             height={120}
@@ -14,14 +16,24 @@ export default function Footer() {
             alt="logo"
             className="relative bottom-12"
           />
+          </Link>
         </div>
       </div>
       {/* twitter icon */}
-      <div className="right-part flex justify-around">
-        <div className="bg-slate-300 overflow-hidden rounded-lg  p-2 h-fit">
-          <Twitter />
+      <div className=" flex justify-around">
+        <div className="bg-slate-300 overflow-hidden rounded-lg p-2 pr-4 h-fit hover:rotate-1">
+          <Link href="https://twitter.com/CapitechNepal">
+
+            <Twitter />
+            </Link>
+
         </div>
-        <div className="text">hello</div>{" "}
+        <div className="text flex flex-col pr-16">
+          <div className="header text-2xl font-bold p-2 flex text-slate-200 drop-shadow-md hover:drop-shadow-xl">
+            Capitech.Nepal
+          </div>
+          <p className="text-sm inline-block px-6">Follow Us On Twitter for more updates!</p>
+        </div>
       </div>
     </section>
   );
