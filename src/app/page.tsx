bdaaa82fb8afbd13a89f50e1b45965646689edc7
iam8uman <iam8uman@gmail.com>
2024-01-07 17:32:40 +0545

    main display bar added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,14 @@
+import DiscBox from "@/components/DiscBox";
 import Footer from "@/components/Footer";
-import { Search } from "lucide-react";
+import Search from "@/components/Search";
 import React from "react";
 
 const page = () => {
   return (
     <>
       <div className="main bg-muted min-h-96">
-        {/* Search bar here  */}
-        <div className="flex grow shadow-md bg-muted mx-40 my-12">
-          <input
-            className="flex h-10 w-full rounded-md bg-slate-40000 text-black px-3 py-2 text-sm placeholder:text-black focus:outline-blue-500 focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-            type="search"
-            placeholder="Serach"
-          />
-        </div>
-
+        <Search />
+        <DiscBox />
         <Footer />
       </div>
     </>
